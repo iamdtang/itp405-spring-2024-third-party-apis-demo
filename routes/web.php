500e0179fedee9b6fc2cc2539d5e13846d5e117e
David Tang <687449+iamdtang@users.noreply.github.com>
2024-03-21 20:43:32 -0700

    Yelp has a REST API called Yelp Fusion: https://www.yelp.com/developers.
    Click on "Yelp Fusion" and then "Manage API Access".
    
    First, you will need to create an application to get access to an API Key. This should be under "General". Once you have that key, create an environment variable called `YELP_API_KEY`:
    
    We will be sending our API key in our requests under the `Authorization` header. See https://www.yelp.com/developers/documentation/v3/authentication for more details.
    
    In this PR, we will use the Business Search API: https://www.yelp.com/developers/documentation/v3/business_search.

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,24 @@ Route::get('/itunes', function (Request $request) {
         'response' => $response,
     ]);
 });
+
+Route::get('/yelp', function () {
+    $queryString = http_build_query([
+        'term' => 'vegan',
+        'location' => 'Los Angeles',
+    ]);
+
+    // We can set headers for our request using withHeaders
+    // https://laravel.com/docs/11.x/http-client#headers
+    return Http::withHeaders([
+        'Authorization' => "Bearer " . env('YELP_API_KEY')
+    ])
+        ->get("https://api.yelp.com/v3/businesses/search?$queryString")
+        ->json();
+
+    // OR you can use withToken to set the Authorization header
+    // https://laravel.com/docs/11.x/http-client#bearer-tokens
+    return Http::withToken(env('YELP_API_KEY'))
+        ->get("https://api.yelp.com/v3/businesses/search?$queryString")
+        ->json();
+});
